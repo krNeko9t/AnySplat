5a6e0166770b950a02096ec49de39a2906726a46
krNeko9t <1016163456@qq.com>
2026-03-19 07:01:45 +0800

    tmp

--- tmp.py
+++ tmp.py
@@ -4,16 +4,18 @@ LPIPS(net="vgg")
 python iggt_idmap.py \
     --image_dir 3dovs/bench/images \
     --model_path /mnt/shared-storage-gpfs2/solution-gpfs02/liaoyuanjun/iggt_checkpoint.pth \
-    --output_dir output_idmap \
-    --image_size 504 336 \
-    --batch_size 4 \
-    --knn_k 20 \
+    --output_dir output_idmap_bench_fin \
+    --image_size 504,336 \
+    --batch_size 48 \
     --eps 0.06 \
-    --min_samples 100 \
-    --min_cluster_size 500
+    --hdbscan_min_samples 100 \
+    --hdbscan_min_cluster_size 500 \
+    --knn_k 60 \
+    --spatial_weight 0.0 \
+    --max_cluster_points 200000
 
 python iggt_idmap.py \
-    --image_dir 3dovs/bench/images \
+    --image_dir infinigen/16255241/frames/Image/camera_0 \
     --model_path /mnt/shared-storage-gpfs2/solution-gpfs02/liaoyuanjun/iggt_checkpoint.pth \
     --output_dir output_idmap_bench_sw0_kk60 \
     --image_size 504,336 \
@@ -24,13 +26,3 @@ python iggt_idmap.py \
     --knn_k 60 \
     --spatial_weight 0.0 \
     --max_cluster_points 600000
-
-python iggt_idmap.py \
-    --image_dir infinigen/16255241/frames/Image/camera_0 \
-    --model_path /mnt/shared-storage-gpfs2/solution-gpfs02/liaoyuanjun/iggt_checkpoint.pth \
-    --output_dir output_idmap2 \
-    --image_size 504,336 \
-    --batch_size 48 \
-    --eps 0.06 \
-    --hdbscan_min_samples 100 \
-    --hdbscan_min_cluster_size 500
